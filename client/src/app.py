4bdd53fd0a8c014d50f954fe681d1379fd0009c4
ethmth <ethan@smaze.net>
2023-03-13 02:13:23 -0400

    Added python-hosts management to client

--- client/src/app.py
+++ client/src/app.py
@@ -1,6 +1,25 @@
 import socketio
+import requests
+from python_hosts import Hosts, HostsEntry
 
 API_URL = "http://localhost:5000"
+
+hosts = Hosts(path='hosts.txt')
+
+r = requests.get(url = f"{API_URL}/ips")
+
+for ip in r.json():
+    try:
+        entry = HostsEntry(entry_type='ipv4', address=f"{ip['ip']}", names=[f"{ip['id']}.remote"])
+        hosts.add([entry])
+        hosts.write()
+        print(ip)
+    except:
+        pass
+
+
+exit
+
 sio = socketio.Client()
 
 @sio.event
