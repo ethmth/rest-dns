8ab0d8504bad49bd91e043d696f0590064dd080f
ethmth <ethan@smaze.net>
2023-03-13 21:17:40 -0400

    Updated host script

--- host/post.sh
+++ host/post.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-LOCAL_IP="192.168.86.57"
+# GET ABSOLUTE PATH
+SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
+cd $SCRIPT_RELATIVE_DIR
+ABSOLUTE_PATH=$(pwd)
+
+source "${ABSOLUTE_PATH}/.env"
+LOCAL_IP="192.168.86.55"
 
 curl -o /dev/null \
 	-X POST -H "Content-Type: application/json" \
  	-d "{\"id\": \"$(cat /etc/hostname)\", \"ip\": \"$(piactl get vpnip)\", \"port\": \"$(piactl get portforward)\", \"local\":\"${LOCAL_IP}\"}" \
-	http://localhost:5000/ip
+	$API_URL/ip
